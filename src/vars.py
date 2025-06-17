00ced66b79b7a9313a0d32b53139dd06e84daaed
Sacha Hibon <sachaedgar@yahoo.fr>
2025-06-17 17:34:04 +0200

    feat: vars.py

--- src/vars.py
+++ src/vars.py
@@ -5,4 +5,5 @@ import tmp as tmp_module
 
 PATH_TMP = Path(tmp_module.__file__).parent
 PATH_DOCS = Path(docs_module.__file__).parent
+PATH_DOCS_PASCALE = PATH_DOCS / "pascal"
 PATH_CONFIG_FILE = PATH_TMP / "fichier_configuration.xlsx"
