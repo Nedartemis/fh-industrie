4ae7c25c5dab7908025dac7c40828107efe5066d
Sacha Hibon <sachaedgar@yahoo.fr>
2025-09-23 00:38:47 +0200

    fix: vars from deployment

--- src/vars.py
+++ src/vars.py
@@ -4,15 +4,23 @@ from typing import Callable
 import docs as docs_module
 import tmp as tmp_module
 
-PATH_TMP = Path(tmp_module.__file__).parent
+# path
+try:
+    PATH_TMP = Path(tmp_module.__file__).parent
+except:
+    PATH_TMP = Path("tmp/")
+
 PATH_ROOT = PATH_TMP.parent.resolve()
-PATH_DOCS = Path(docs_module.__file__).parent
+
+PATH_DOCS = PATH_ROOT / "docs"
 PATH_DOCS_PASCALE = PATH_DOCS / "pascal"
-PATH_TEMPLATE = PATH_DOCS.parent / "templates"
-PATH_CACHE = PATH_DOCS.parent / "cache"
-PATH_TEST = PATH_DOCS.parent / "test"
+
+PATH_TEMPLATE = PATH_ROOT / "templates"
+PATH_CACHE = PATH_ROOT / "cache"
+PATH_TEST = PATH_ROOT / "test"
 PATH_CONFIG_FILE = PATH_TEST / "config_file.xlsx"
 
+# logger
 TYPE_LOGGER = Callable[[str], None]
 DEFAULT_LOGGER = print
 LAZY_LOGGER = lambda _: None
